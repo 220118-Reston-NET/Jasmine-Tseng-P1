42ae5985074def08c92cd8fdaf838f312ac833d1
JasmineTseng7 <97990706+JasmineTseng7@users.noreply.github.com>
2022-03-02 07:54:54 -0800

    fix

--- StoreApi/Controllers/StoreController.cs
+++ StoreApi/Controllers/StoreController.cs
@@ -102,7 +102,7 @@ namespace StoreApi.Controllers
         {
             try
             {
-                p_orderby.ToLower();
+                //p_orderby.ToLower();
 
                 List<OrderDetails> _listOrderDetail = new List<OrderDetails>();
                 List<OrderDetails> _sortedOrdersList = new List<OrderDetails>();
@@ -132,7 +132,7 @@ namespace StoreApi.Controllers
                     });
                 }
 
-                switch (p_orderby)
+                switch (p_orderby.ToLower())
                 {
                     case "date":
                         Log.Information("User looked at orders history by date created");
